f278d79f8fad0088035ef9275f72f69040fec61a
EnderLab <enderlabo@gmail.com>
2020-10-21 21:06:42 -0500

    need to fix npm movieTrailer

--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
+import YouTube from 'react-youtube';
 import axios from '../helpers/axios';
+import movieTrailer from 'movie-trailer';
 
 import './row.css';
 
@@ -7,6 +9,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
 
     const base_url = "https://image.tmdb.org/t/p/original/";
     const [ movies, setMovies ] = useState([]);
+    const [ trailerUrl, setTrailerUrl ] = useState("");
 
     
     useEffect( () => {
@@ -20,9 +23,31 @@ function Row({ title, fetchUrl, isLargeRow }) {
 
         fetchData();
 
-    }, [ movies, fetchUrl ]);
+    }, [ fetchUrl ]);
 
+    console.log(movies)
+    const opts = {
+        height: "390",
+        width: "100%",
+        playerVars: {
+            autoplay: 1
+        }
+    }
     
+    const handleClick = ( movie ) => {
+
+        if( trailerUrl ){
+            setTrailerUrl("");
+        } else {
+            movieTrailer( movie?.name || "" )
+            .then( (base_url) => {
+                //get params from url https://www.youtube.com/watch?v=BBe2BvssBgY
+                const urlParams = new URLSearchParams( new URL( base_url ).search );
+                setTrailerUrl( urlParams.get('v'))
+            })
+            .catch( err => console.log(err) )
+        }
+    }
 
     return (
         <div className="row">
@@ -31,13 +56,14 @@ function Row({ title, fetchUrl, isLargeRow }) {
             <div className="row__posters">
             {
                 movies.map( movie => (
-                    <img key={ movie.id } className={`row__poster ${ isLargeRow && "row__posterLarge"} `} 
+                    <img key={ movie.id } className={`row__poster ${ isLargeRow && "row__posterLarge"} `}
+                    onClick={ handleClick( movie ) } 
                     src={ `${ base_url }${ isLargeRow ? movie.poster_path : movie.backdrop_path }` } 
                     alt={ movie.name } />
                 ))
             }
             </div>
-
+            { trailerUrl && <YouTube videoId={ trailerUrl } opts={ opts } />}
         </div>
     )
 }
